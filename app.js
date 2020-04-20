9ff401fdbf561d3a6cdc0ad78f27b94d7d01c636
TomiSalo <tomi.a.salo@outlook.com>
2020-04-20 18:57:42 +0300

    Ensimmäinen versio valmis.

--- app.js
+++ app.js
@@ -3,28 +3,79 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        playerStrike: Math.round(Math.random() * 11),
-        monsterStrike: Math.round(Math.random() * 10),
-        gameIsRunning: false
+        playerStrike: [],
+        monsterStrike: [],
+        gameIsRunning: false,
+       
     },
     methods: {
 
         startGame: function() {
             this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.playerStrike = [];
+            this.monsterStrike = [];
+           
         },
         attack: function() {
-           
-           this.monsterHealth = this.monsterHealth - this.playerStrike;
-           this.playerHealth = this.playerHealth - this.monsterStrike;
+           var playerStrike = Math.round(Math.random() * 11);
+           this.playerStrike.push(playerStrike);
+           var monsterStrike = Math.round(Math.random() * 11) +1;
+           this.monsterStrike.push(monsterStrike);
+           this.monsterHealth = this.monsterHealth - playerStrike;
+           this.playerHealth = this.playerHealth - monsterStrike;
+           console.log(this.playerStrike)
            if(this.playerHealth < 1) {
                alert('YOU LOSE!');
+               this.gameIsRunning = false;
            } 
            else if(this.monsterHealth < 1) {
             alert('YOU WIN!!');
+            this.gameIsRunning = false;
            }
+        },
+        giveUp: function() {
+            this.gameIsRunning = false;
+            this.playerStrike = [];
+            this.monsterStrike = [];
+            
            
-         
-        }
+        },
+
+        specialAttack: function() {
+            var playerStrike = Math.round(Math.random() * 15) + 5;
+            this.playerStrike.push(playerStrike);
+            var monsterStrike = Math.round(Math.random() * 11) +1;
+            this.monsterStrike.push(monsterStrike);
+            this.monsterHealth = this.monsterHealth - playerStrike;
+            this.playerHealth = this.playerHealth - monsterStrike;
+            if(this.playerHealth < 1) {
+                alert('YOU LOSE!');
+                this.gameIsRunning = false;
+            } 
+            else if(this.monsterHealth < 1) {
+             alert('YOU WIN!!');
+             this.gameIsRunning = false;
+            }
+        },
+
+        heal: function() {
+            if(this.playerHealth <= 90) {
+                this.playerHealth += 10;
+                var monsterStrike = Math.round(Math.random() * 11) +1;
+                this.monsterStrike.push(monsterStrike);
+                this.playerHealth = this.playerHealth - monsterStrike;
+            } else {
+                this.playerHealth = 100;
+                var monsterStrike = Math.round(Math.random() * 11) +1;
+                this.monsterStrike.push(monsterStrike);
+                this.playerHealth = this.playerHealth - monsterStrike;
+            }
+                
+            
+            }
+        
     }
 });
 
